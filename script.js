780634af81713245720a38e361249ac36709af31
HexaA2 <hexatonysong@gmail.com>
2024-07-30 10:51:27 -0400

    Added reviews and tabs

--- script.js
+++ script.js
@@ -42,4 +42,41 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     });
 
+
+
+    function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  if (evt) {
+    evt.currentTarget.className += " active";
+  } else {
+    // Set the default tab as active if no event is provided
+    document.querySelector(`.tablinks[onclick="openTab(event, '${tabName}')"]`).className += " active";
+  }
+}
+
+// Automatically open the first tab by default on page load
+document.addEventListener("DOMContentLoaded", function() {
+  var firstTab = document.querySelector('.tab .tablinks');
+  if (firstTab) {
+    var firstTabName = firstTab.getAttribute('onclick').match(/'([^']+)'/)[1];
+    openTab(null, firstTabName);
+  }
+});
+
     
