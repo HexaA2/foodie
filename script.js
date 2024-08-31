b7119512547e775ed7630cffcbacf34875ae1bbe
HexaA2 <hexatonysong@gmail.com>
2024-08-31 17:51:12 -0400

    fix

--- script.js
+++ script.js
@@ -79,13 +79,12 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-document.querySelector('tbody').addEventListener('click', function(event) {
-    // Find the closest <tr> element
-    const row = event.target.closest('tr');
-    
-    // Check if the row has a data-url attribute and navigate to it
-    if (row && row.dataset.url) {
-        window.location.href = row.dataset.url;
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#main_course_table tbody').addEventListener('click', function(event) {
+      const row = event.target.closest('tr');
+      if (row && row.dataset.url) {
+          window.location.href = row.dataset.url;
+      }
+  });
 });
 
